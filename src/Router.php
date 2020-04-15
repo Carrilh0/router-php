68c6100b159f1596c6d2661f9aa3f3ceadf7713b
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-04-15 04:42:21 -0300

    metodos e atributos da classe Router add

--- src/Router.php
+++ src/Router.php
@@ -1,11 +1,50 @@
 <?php
-
-namespace src;
-
-use src\Dispacher;
-use src\RouteCollection;
-
-class RouteCollection
+ 
+namespace Src;
+ 
+use Src\Request;
+use Src\Dispacher;
+use Src\RouteCollection;
+ 
+class Router 
 {
-    
+     
+    protected $routeCollection;
+    protected $dispacher;
+
+    public function __construct()
+    {
+        $this->routeCollection = new RouteCollection;
+        $this->dispacher = new Dispacher;
+    }
+
+    public function get($pattern, $callback)
+    {
+        $this->routeCollection->add('get', $pattern, $callback);
+        return $this;
+    }
+
+    public function post($pattern, $callback)
+    {
+        $this->routeCollection->add('post', $pattern, $callback);
+        return $this;
+    }
+
+    public function put($pattern, $callback)
+    {
+        $this->routeCollection->add('put', $pattern, $callback);
+        return $this;
+    }
+
+    public function delete($pattern, $callback)
+    {
+        $this->routeCollection->add('delete', $pattern, $callback);
+        return $this;
+    }
+
+    public function find($requestType, $pattern)
+    {
+        return $this->routeCollection->where($requestType);
+    }
+
 }
